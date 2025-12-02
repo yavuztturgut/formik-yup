fe7ad579710d621f82c6dc05e7218625e77053a9
Yavuz <yavuz2809@gmail.com>
2025-12-02 12:51:51 +0300

    feat: emptyData default data created

---
+++ src/emptyData.js
@@ -0,0 +1,18 @@
+const emptyData = [
+    {
+        id: 1, // Benzersiz ID
+        username: "yavuz",
+        email: "yavuz@example.com",
+        password: "123", // Gerçek uygulamada parola saklanmaz
+        confirmPassword: "123",
+        age: "21"
+    },
+    {
+        id: 2, // Benzersiz ID
+        username: "ibo",
+        email: "ibo@example.com",
+        password: "123",
+        confirmPassword: "123",
+        age: "23"
+    },
+];
